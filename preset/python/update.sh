9277008aaee5582f975354793d87bfdfc150d923
ngicks <yknt.bsl@gmail.com>
2025-02-26 02:08:29 +0900

    👷: remove unnecessary sudo

--- preset/python/update.sh
+++ preset/python/update.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 ~/.local/bin/uv python install ${VER}
 # uv creates ~/.local/bin/ dir
-sudo ln -sf $(~/.local/bin/uv python find ${VER}) ~/.local/bin/python3
-sudo ln -sf python3 ~/.local/bin/python
+ln -sf $(~/.local/bin/uv python find ${VER}) ~/.local/bin/python3
+ln -sf python3 ~/.local/bin/python
