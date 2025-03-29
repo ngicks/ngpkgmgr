8e162910d5e6528f88f5c83c1db8c7a6b39140f7
ngicks <yknt.bsl@gmail.com>
2025-03-30 05:37:58 +0900

    👷: change nvim install location

--- preset/basetool/nvim/install.sh
+++ preset/basetool/nvim/install.sh
@@ -4,7 +4,7 @@ pushd /tmp
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-${OS}-$(uname -m).tar.gz
 rm -rf nvim-linux-x86_64
 tar -xzf nvim-linux-x86_64.tar.gz
-rm -rf ~/nvim
-mv ./nvim-linux-x86_64 ~/nvim
+rm -rf ~/.local/nvim
+mv ./nvim-linux-x86_64 ~/.local/nvim
 popd
 echo 'export PATH="$PATH:$HOME/nvim/bin"' >> ~/home.sh
