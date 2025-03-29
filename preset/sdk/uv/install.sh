3982286ffccedb5e1265cd0ff24ad560deca7954
ngicks <yknt.bsl@gmail.com>
2025-03-30 06:44:25 +0900

    👷: fix env

--- preset/sdk/uv/install.sh
+++ preset/sdk/uv/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -LsSf https://astral.sh/uv/install.sh | NO_MODIFY_PATH=1 sh
+curl -LsSf https://astral.sh/uv/install.sh | INSTALLER_NO_MODIFY_PATH=1 sh
 echo '. "$HOME/.local/bin/env"' >> ${PROFILE_SH}
