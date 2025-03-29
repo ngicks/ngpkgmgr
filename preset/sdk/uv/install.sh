8acdea7032fca2dc53ae1dd25494fbeed674e106
ngicks <yknt.bsl@gmail.com>
2025-03-30 07:11:00 +0900

    👷: fix env

--- preset/sdk/uv/install.sh
+++ preset/sdk/uv/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 curl -LsSf https://astral.sh/uv/install.sh | INSTALLER_NO_MODIFY_PATH=1 sh
-echo '. "$HOME/.local/bin/env"' >> ${PROFILE_SH}
+
+echo 'case ":${PATH}:" in
+    *:"$HOME/.local/bin":*)
+        ;;
+    *)
+        # Prepending path in case a system-installed binary needs to be overridden
+        export PATH="$HOME/.local/bin:$PATH"
+        ;;
+esac
+' >> ${PROFILE_SH}
