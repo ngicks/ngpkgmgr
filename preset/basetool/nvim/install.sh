6cd0052c9576819f5b764bf894b20387fcaa6f31
ngicks <yknt.bsl@gmail.com>
2025-03-30 06:18:10 +0900

    👷: rebuild

--- preset/basetool/nvim/install.sh
+++ preset/basetool/nvim/install.sh
@@ -7,4 +7,4 @@ tar -xzf nvim-linux-x86_64.tar.gz
 rm -rf ~/.local/nvim
 mv ./nvim-linux-x86_64 ~/.local/nvim
 popd
-echo 'export PATH="$PATH:$HOME/nvim/bin"' >> ~/home.sh
+echo 'export PATH="$PATH:$HOME/nvim/bin"' >> ${PROFILE_SH}
