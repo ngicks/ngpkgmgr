b5cb0c24769c5250313e71e59299e6b07153851d
ngicks <yknt.bsl@gmail.com>
2025-03-30 22:44:30 +0900

    👷: fix nvim path

--- preset/basetool/nvim/install.sh
+++ preset/basetool/nvim/install.sh
@@ -7,4 +7,4 @@ tar -xzf nvim-linux-x86_64.tar.gz
 rm -rf ~/.local/nvim
 mv ./nvim-linux-x86_64 ~/.local/nvim
 popd
-echo 'export PATH="$PATH:$HOME/nvim/bin"' >> ${PROFILE_SH}
+echo 'export PATH="$PATH:$HOME/.local/nvim/bin"' >> ${PROFILE_SH}
