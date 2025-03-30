34f733f0ebb45882d48f21ebbd59926e198c34b4
ngicks <yknt.bsl@gmail.com>
2025-03-30 14:44:40 +0900

    👷: fix nvim update location

--- preset/basetool/nvim/update.sh
+++ preset/basetool/nvim/update.sh
@@ -4,7 +4,7 @@ pushd /tmp
 curl -LO https://github.com/neovim/neovim/releases/download/v${VER}/nvim-${OS}-$(uname -m).tar.gz
 rm -rf nvim-linux-x86_64
 tar -xzf nvim-linux-x86_64.tar.gz
-rm -rf ~/nvim
-mv ./nvim-linux-x86_64 ~/nvim
+rm -rf ~/.local/nvim
+mv ./nvim-linux-x86_64 ~/.local/nvim
 popd
 
